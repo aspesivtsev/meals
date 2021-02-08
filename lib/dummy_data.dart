70aee2cfed906125b894a52c90eef4dca188d6d9
anatoly <anspesiv@yahoo.com>
2021-02-08 17:55:40 +0300

    adding GridView and model

---
+++ lib/dummy_data.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Quick & Easy',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Hamburgers',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c4',
+    title: 'German',
+    color: Colors.amber,
+  ),
+];
