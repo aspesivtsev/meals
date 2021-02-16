6acc83ed83d857deab2971c61918ac52df7a50ae
anatoly <anspesiv@yahoo.com>
2021-02-16 23:01:47 +0300

    working with fonts

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
       ),
+
       //home: MyHomePage(),
       home: CategoriesScreen(),
     );
