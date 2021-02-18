c3a0fe92f2bb9b0223009c080c32386ad4733f41
Anatoly <anatoly@MacBook-Pro-Anatoly.local>
2021-02-19 00:10:55 +0300

    added splash on category tap

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -7,21 +7,25 @@ class CategoryItem extends StatelessWidget {
   CategoryItem(this.title, this.color);
   @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: const EdgeInsets.all(15),
-        child: Text(
-          title,
-          style: Theme.of(context).textTheme.headline6,
-        ),
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                color.withOpacity(0.4),
-                color,
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-            borderRadius: BorderRadius.circular(10)));
+    return InkWell(
+      onTap: () => null,
+      splashColor: Theme.of(context).primaryColor,
+      child: Container(
+          padding: const EdgeInsets.all(15),
+          child: Text(
+            title,
+            style: Theme.of(context).textTheme.headline6,
+          ),
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  color.withOpacity(0.4),
+                  color,
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              borderRadius: BorderRadius.circular(10))),
+    );
   }
 }
--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -6,6 +6,9 @@ class CategoryMealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('The recipes'),
+      ),
       body: Center(
         child: Text('The new screen'),
       ),
