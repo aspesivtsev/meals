d28646583ae85299d08f50bcfff76904cab24feb
Anatoly <anatoly@MacBook-Pro-Anatoly.local>
2021-03-17 23:52:04 +0300

    added ingredients in a ListView

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -36,9 +36,24 @@ class MealDetailScreen extends StatelessWidget {
             ),
           ),
           Container(
+            decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(10)),
             height: 200,
             width: mediaQuery.size.width,
-            child: ListView(),
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
+            child: ListView.builder(
+              itemBuilder: (ctx, index) => Card(
+                color: Theme.of(context).accentColor,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(selectedMeal.ingredients[index]),
+                ),
+              ),
+              itemCount: selectedMeal.ingredients.length,
+            ),
           ),
         ],
       ),
